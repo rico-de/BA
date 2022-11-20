82b1362ee2578926ccbf9555f233a1c5809f1e28
Rico Ukro <rico.ukro@gmx.de>
2022-11-20 17:40:15 +0100

    added generic makefile

---
+++ 1_semester/IMPPR/c-generic/main.c
@@ -0,0 +1,14 @@
+/* 
+    Info:     ANSI C example 
+    Version:  0.1 
+    Author:   Rico Ukro, Kesselsdorf, Germany
+*/
+
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+    printf("Hello world!\n");
+
+    return 0;
+}
+
